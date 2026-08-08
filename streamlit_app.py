f65cee508ac009a5d2c7c70eb797947ed4dbc966
chocolaflavour-coder <chocolaflavour@gmail.com>
2026-08-08 13:41:59 +0300

    Add comment for saving and running check_syntax.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 from streamlit.errors import StreamlitSecretNotFoundError
 # save as check_syntax.py ثم شغّل: python check_syntax.py
+# save as check_syntax.py ثم شغّل: python check_syntax.py
 import ast, sys
 
 fname = "streamlit_app.py"
